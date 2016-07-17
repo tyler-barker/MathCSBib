2af7693b4a8f08e5547e4b0ddab1f58fa3a10ab8
tyler-barker <tbarker@tulane.edu>
2016-07-16 19:07:03 -0500

    Starting to add publications

--- MathCSBib/src/main/java/com/barker/controller/PublicationController.java
+++ MathCSBib/src/main/java/com/barker/controller/PublicationController.java
@@ -1,16 +1,41 @@
 package com.barker.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.barker.dao.PublicationDAO;
+import com.barker.model.Publication;
 
 @Controller
 @RequestMapping("/publications")
 public class PublicationController {
 	
+	@Autowired
+	private ApplicationContext ctx;
+	
+	@RequestMapping(value="/newPublication", method=RequestMethod.GET)
+	public String getNewPublicationForm(Model model) {
+		model.addAttribute("newPublication", new Publication());
+		return "NewPublicationsForm";
+	}
+	
+	@RequestMapping(value="/newPublication", method=RequestMethod.POST)
+	public String addAuthor(@ModelAttribute("newPublication") Publication newPublication, Model model) {
+		model.addAttribute("newPublication", newPublication);
+		PublicationDAO pubDAO = ctx.getBean(PublicationDAO.class);
+		pubDAO.save(newPublication);
+		return "PublicationAdded";
+	}
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public String getPublications(Model model) {
+		PublicationDAO pubDAO = ctx.getBean(PublicationDAO.class);
+		model.addAttribute("publications", pubDAO.getPublications());
 		return "PublicationsPage";
 	}
 	
