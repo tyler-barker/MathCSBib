3a1d1f32e0901cc7312ca6851edab799d8133ee3
tyler-barker <tbarker@tulane.edu>
2016-07-17 16:26:16 -0500

    Added Picture to Author page.

--- MathCSBib/src/main/java/com/barker/MathCsBibApplication.java
+++ MathCSBib/src/main/java/com/barker/MathCsBibApplication.java
@@ -30,12 +30,14 @@ public class MathCsBibApplication {
 		author.setLastName("Barker");
 		author.setMiddleInitial("C");
 		author.setUniversity("Tulane");
+		author.setPicture("http://cs.tulane.edu/~tbarker/images-static/Me.jpg");
 		authorDAO.save(author);
 		
 		Author author2 = new Author();
 		author2.setFirstName("Michael");
 		author2.setLastName("Mislove");
 		author2.setUniversity("Tulane");
+		//author2.setPicture("http://www.cs.tulane.edu/~mwm/Michael_Misloves_Home_Page/Michael_Mislove_files/droppedImage.jpg");
 		authorDAO.save(author2);
 		
 		pub.setTitle("A Monad for Randomized Algorithms");
