09d715192cfa3a419eea8a4fb59e9acd1c691cd6
tyler-barker <tbarker@tulane.edu>
2016-07-29 22:34:19 -0500

    Added a simple user page

--- MathCSBib/src/main/java/com/barker/controller/CurrentUserControllerAdvice.java
+++ MathCSBib/src/main/java/com/barker/controller/CurrentUserControllerAdvice.java
@@ -1,4 +1,4 @@
-package com.barker;
+package com.barker.controller;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 
 @ControllerAdvice
 public class CurrentUserControllerAdvice {
-    @ModelAttribute("currentUser")
-    public UserDetails getCurrentUser(Authentication authentication) {
-        return (authentication == null) ? null : (UserDetails) authentication.getPrincipal();
-    }
+	@ModelAttribute("currentUser")
+	public UserDetails getCurrentUser(Authentication authentication) {
+		return (authentication == null) ? null : (UserDetails) authentication.getPrincipal();
+	}
+
+	@ModelAttribute("title")
+	public String addTitle() {
+		return "MathCSBib";
+	}
 }
--- MathCSBib/src/main/java/com/barker/controller/UserController.java
+++ MathCSBib/src/main/java/com/barker/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.barker.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -41,5 +43,12 @@ public class UserController {
 			result.rejectValue("username", "error.alreadyExists", "This username already exists.");
 			return "register";
 		}
+	}
+	
+	@RequestMapping(value="/user", method=RequestMethod.GET)
+	public String getUser(Model model, HttpServletRequest request) {
+		User currentUser = userRepo.findOneByUserName(request.getUserPrincipal().getName());
+		model.addAttribute("currentUser", currentUser);
+		return "UserPage";
 	}
 }
